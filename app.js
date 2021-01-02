639dac0a7ac2bd8dba3c7d12ea74d99c16889622
Ris3r <ProjectGitHub72@gmail.com>
2021-01-02 16:30:05 +0545

    Add category,product and user model and CRUD routes and controllers

--- app.js
+++ app.js
@@ -4,19 +4,22 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const expressValidator = require("express-validator");
+const cors = require("cors");
 
 require("dotenv").config();
 const app = express();
 
-
 //route import
+const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
+const productRoutes = require("./routes/product");
 
 mongoose
   .connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true
+    useCreateIndex: true,
   })
   .then(() => console.log("Db connected successfully"));
 
@@ -25,11 +28,15 @@ app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(expressValidator());
+app.use(cors());
 
 //routes middleware
+app.use("/api", authRoutes);
 app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
   console.log("Server started");
