451bd6629bf780bcaae6bb27c60b8b3e125b6c37
Ris3r <ProjectGitHub72@gmail.com>
2021-01-17 19:08:03 +0545

    Add mongodb atlas

--- app.js
+++ app.js
@@ -17,14 +17,14 @@ const productRoutes = require("./routes/product");
 const braintreeRoutes = require("./routes/braintree");
 const orderRoutes = require("./routes/order");
 
-
 mongoose
   .connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
   })
-  .then(() => console.log("Db connected successfully"));
+  .then(() => console.log("Db connected successfully"))
+  .catch((err) => console.log(err));
 
 //middlewares
 app.use(morgan("dev"));
@@ -40,7 +40,6 @@ app.use("/api", categoryRoutes);
 app.use("/api", productRoutes);
 app.use("/api", braintreeRoutes);
 app.use("/api", orderRoutes);
-
 
 const port = process.env.PORT || 8000;
 
